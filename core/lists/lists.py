9c63fdaab856707f161ff50a4d37ca4b97ffcdbf
David Kingman <David.Kingman@hmtreasury.gov.uk>
2024-08-19 17:12:29 +0100

    Updated lists

--- core/lists/lists.py
+++ core/lists/lists.py
@@ -7,12 +7,18 @@ mod.list("letter", "The spoken phonetic alphabet")
 mod.list("website", "Custom user websites")
 mod.list("punctuation", "Custom keys for use with dictation commands")
 mod.list("symbol_key", "Custom keys for use outside of dictation commands")
+mod.list("special_key", "Custom keys for use in commands")
+mod.list("address_book", "My work email addresses")
+mod.list("file_extensions", "File extensions")
 
 # R lists
 mod.list("r_packages", "R language code libraries")
 mod.list("r_functions", "R functions")
 mod.list("r_function_arguments", "R function arguments")
 mod.list("r_symbols", "R symbols")
+
+# Talon programming lists
+mod.list("talon_functions", "Talon functions")
 
 # Git
 mod.list("git_functions", "Git commands")
