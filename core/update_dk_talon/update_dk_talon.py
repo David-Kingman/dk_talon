60a8a8e7781e68e3e2114b9a839da9e59ddb62f1
David Kingman <David.Kingman@hmtreasury.gov.uk>
2024-08-20 14:59:07 +0100

    Added update mousectrl

--- core/update_dk_talon/update_dk_talon.py
+++ core/update_dk_talon/update_dk_talon.py
@@ -12,3 +12,10 @@ class Actions:
         shutil.rmtree(destination_dir)
         shutil.copytree(source_dir, destination_dir, ignore=shutil.ignore_patterns(".git*"))
         app.notify("Update Completed")
+
+    def update_mouse_settings():
+        "Backs up my Kinesic Mouse Live commands"
+        source = r"C:/Users/dkingman/AppData/Local/Xcessity Software Solutions/KinesicMouse Live/settings.xml"
+        destination = r"F:/Projects/DavidKingman/dk_talon/settings"
+        shutil.copy(source, destination)
+        app.notify("Mouse Updated")
--- core/update_dk_talon/update_dk_talon.talon
+++ core/update_dk_talon/update_dk_talon.talon
@@ -5,6 +5,9 @@ mode: command
 # Update Talon/user
 update: user.update_dk_talon()
 
+# Update Kinesic Mouse Live settings
+update mouse: user.update_mouse_settings()
+
 # Save file and update talon
 shave:
     edit.save()
