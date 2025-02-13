fc179527fb31d22f804ff938eb7ecb10b093a47a
David-Kingman <david_kingman@hotmail.co.uk>
2025-02-13 20:12:01 +0000

    Deleted mouse backup commands

--- core/update_dk_talon/update_dk_talon.py
+++ core/update_dk_talon/update_dk_talon.py
@@ -13,14 +13,6 @@ class Actions:
         shutil.copytree(source_dir, destination_dir, ignore = shutil.ignore_patterns(".git*"))
         app.notify("Update Completed")
 
-    def update_subfolders(origin: str, destination: str):
-        "Copies specific files across from the F: drive to talon/user"
-        source_dir ="F:/Projects/DavidKingman/talon_voice_repos/" + origin
-        destination_dir ="C:/Users/dkingman/AppData/Roaming/talon/user/" + destination
-        shutil.rmtree(destination_dir)
-        shutil.copytree(source_dir, destination_dir, ignore = shutil.ignore_patterns(".git*"))
-        app.notify("Update Completed")
-
     def update_file(origin_file: str, destination_folder: str):
         "Copies individual files from my personal repo to talon/user"
         if os.path.exists(origin_file) and os.path.exists(destination_folder):
@@ -28,10 +20,4 @@ class Actions:
             app.notify("Update Completed")
         else:
             app.notify("Update Failed: File Doesn't Exist!")
-    
-    def update_mouse_settings():
-        "Backs up my Kinesic Mouse Live commands"
-        source = r"C:/Users/dkingman/AppData/Local/Xcessity Software Solutions/KinesicMouse Live/settings.xml"
-        destination = r"F:/Projects/DavidKingman/dk_talon/settings"
-        shutil.copy(source, destination)
-        app.notify("Mouse Updated")
+            
--- core/update_dk_talon/update_dk_talon.talon
+++ core/update_dk_talon/update_dk_talon.talon
@@ -2,12 +2,10 @@ os: windows
 mode: command
 -
 
-# Update Talon/user
+# Update Talon custom file sets on work computer
 ^update$: user.update_custom_filesets("dk_talon")
 ^update community$: user.update_custom_filesets("community")
 ^update cursorless$: user.update_custom_filesets("cursorless-talon")
-^update words$: user.update_file("dk_talon/csv/words_to_replace.csv", "community/settings")
-^update app names$: user.update_file("dk_talon/csv/app_name_overrides.windows.csv", "community/core/app_switcher")
 
 # Update words_to_replace files
 ^update words$: 
@@ -23,5 +21,3 @@ mode: command
 ^update home names$: 
     user.update_file("C:/Users/david/AppData/Roaming/talon/user/dk_talon/csv/app_name_overrides.windows.csv", "C:/Users/david/AppData/Roaming/talon/user/community/core/app_switcher")
         
-# Update Kinesic Mouse Live settings
-^update mouse$: User.update_mouse_settings()
